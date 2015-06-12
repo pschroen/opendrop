f72fd5f438322ab503890d8892789c02e8144267
Patrick Schroen <ps@ufotechnologies.com>
2015-06-12 19:39:09 -0400

    clean.sh

---
+++ clean.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+bucket="opendrop"
+
+clean=""
+list=$(aws s3 ls s3://$bucket --recursive)
+while read -r line; do
+    fromdate=$(echo "$line" | awk '{print $1,$2}')
+    todate=$(date -d "$fromdate 1 day" +"%s")
+    now=$(date +"%s")
+    if [ $now -ge $todate ]; then
+        path=$(echo "$line" | awk '{print substr($0, index($0,$4))}')
+        drop="${path:0:4}"
+        if [[ $clean != *"$drop"* ]]; then
+            if [[ $clean != "" ]]; then
+                clean="$clean"$'\n'
+            fi
+            clean="$clean$drop"
+        fi
+    fi
+done <<< "$list"
+while read -r line; do
+    aws s3 rm s3://$bucket/$line --recursive
+done <<< "$clean"
